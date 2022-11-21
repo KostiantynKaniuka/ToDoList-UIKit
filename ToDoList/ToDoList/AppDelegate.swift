3a83d5957c9f0e0b71352fa8dd81a786e1ec8c2c
heyhocombo <heyhocombo@gmail.com>
2022-11-21 20:23:45 +0200

    Starting up

---
+++ ToDoList/ToDoList/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToDoList
+//
+//  Created by Kostiantyn Kaniuka on 21.11.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = navigationController
+        
+        
+        return true
+    }
+}
+
---
+++ ToDoList/ToDoList/StyleExtension.swift
@@ -0,0 +1,12 @@
+//
+//  StyleExtension.swift
+//  ToDoList
+//
+//  Created by Kostiantyn Kaniuka on 21.11.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let appBackground = UIColor(red: 211/255, green: 211/255, blue: 211/255, alpha: 1)
+}
