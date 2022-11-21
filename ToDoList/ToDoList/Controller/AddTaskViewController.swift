c1c0a047ec08223dcef9c2cb5f8b324f1fa3e9b6
heyhocombo <heyhocombo@gmail.com>
2022-11-22 00:00:49 +0200

    feat: Add new task controller

---
+++ ToDoList/ToDoList/Controller/AddTaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddTaskViewController.swift
+//  ToDoList
+//
+//  Created by Kostiantyn Kaniuka on 21.11.2022.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
--- ToDoList/ToDoList/Controller/MainViewController.swift
+++ ToDoList/ToDoList/Controller/MainViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
     private let segmentControll = UISegmentedControl()
     private let ongoingTaskViewController = OngoingTaskTableViewController()
     private let doneTaskViewController = DoneTaskTableViewController()
+    private let addNewTaskButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,7 @@ class MainViewController: UIViewController {
         style()
         layout()
         adjustSegmentControll()
+        
     }
 }
 
@@ -27,7 +29,14 @@ extension MainViewController {
         view.backgroundColor = .appBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Tasks"
-        
+        //addNewTaskButton
+        let image = UIImage(systemName: "plus") as UIImage?
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .black
+        config.cornerStyle = .capsule
+        config.contentInsets = NSDirectionalEdgeInsets(top: 25, leading: 25, bottom: 25, trailing: 25)
+        addNewTaskButton.configuration = config
+        addNewTaskButton.setImage(image, for: .normal)
         //SegmentControll
         let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         segmentControll.setTitleTextAttributes(titleTextAttributes, for:.normal)
@@ -37,6 +46,9 @@ extension MainViewController {
         segmentControll.layer.borderColor = UIColor.darkGray.cgColor
         segmentControll.selectedSegmentTintColor = UIColor.white
         segmentControll.layer.borderWidth = 1
+        //Action
+        segmentControll.addTarget(self, action: #selector(segmentControlChanged), for: .valueChanged)
+        addNewTaskButton.addTarget(self, action: #selector(addTaskButtonTapped), for: .touchUpInside)
     }
     
     private func layout() {
@@ -49,11 +61,13 @@ extension MainViewController {
         ongoingTaskViewController.view.translatesAutoresizingMaskIntoConstraints = false
         segmentControll.translatesAutoresizingMaskIntoConstraints = false
         bottomContainer.translatesAutoresizingMaskIntoConstraints = false
+        addNewTaskButton.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(segmentControll)
         view.addSubview(bottomContainer)
         view.addSubview(ongoingTaskViewController.view)
         view.addSubview(doneTaskViewController.view)
+        view.addSubview(addNewTaskButton)
         
         //bottom container
         NSLayoutConstraint.activate([
@@ -72,7 +86,12 @@ extension MainViewController {
             doneTaskViewController.view.bottomAnchor.constraint(equalTo: bottomContainer.topAnchor),
             doneTaskViewController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             doneTaskViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            doneTaskViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            doneTaskViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            //Add new task button
+            addNewTaskButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            addNewTaskButton.centerYAnchor.constraint(equalTo: bottomContainer.firstBaselineAnchor)
+            
         ])
     }
     
@@ -81,6 +100,32 @@ extension MainViewController {
             segmentControll.insertSegment(withTitle: section.rawValue, at: index, animated: false)
         }
         segmentControll.selectedSegmentIndex = 0
+        showContainerView(for: .ongoing)
+    }
+    
+    private func showContainerView(for section: MenuSection) {
+        switch section {
+        case .ongoing:
+            ongoingTaskViewController.tableView.isHidden = false
+            doneTaskViewController.tableView.isHidden = true
+        case .done:
+            ongoingTaskViewController.tableView.isHidden = true
+            doneTaskViewController.tableView.isHidden = false
+        }
+    }
+    
+    @objc private func segmentControlChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            showContainerView(for: .ongoing)
+        case 1:
+            showContainerView(for: .done)
+        default:
+            break
+        }
+    }
+    
+    @objc private func addTaskButtonTapped(_ sender: UIButton) {
+        present(AddTaskViewController(), animated: true, completion: nil)
     }
 }
-
