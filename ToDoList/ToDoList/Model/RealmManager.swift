16fa2e7b45f6ce6c04c768964023a64d5fa91284
heyhocombo <heyhocombo@gmail.com>
2022-11-23 00:46:49 +0200

    Feat: Adding Realm

---
+++ ToDoList/ToDoList/Model/RealmManager.swift
@@ -0,0 +1,70 @@
+//
+//  RealmManager.swift
+//  ToDoList
+//
+//  Created by Kostiantyn Kaniuka on 22.11.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager: ObservableObject {
+    private(set) var localRealm: Realm?
+    @Published private(set) var tasks: [Task] = []
+    
+    init() {
+        openRealm()
+        getTasks()
+    }
+    
+    func openRealm() {
+        do {
+            let config = Realm.Configuration(schemaVersion: 1)
+            Realm.Configuration.defaultConfiguration = config
+            localRealm = try Realm()
+        } catch {
+            print("Error openig Realm: \(error)")
+        }
+    }
+    
+    func addTask(taskTitle: String) {
+        if let localRealm = localRealm {
+            do {
+                try localRealm.write {
+                    let newTask = Task(value: ["title": taskTitle, "completed": false])
+                    localRealm.add(newTask)
+                    getTasks()
+                    print("Added new task to Realm: \(newTask)")
+                }
+            } catch {
+                print("Error adding task to Realm: \(error)")
+            }
+        }
+    }
+    
+    func getTasks() {
+        if let localRealm = localRealm {
+            let allTasks = localRealm.objects(Task.self).sorted(byKeyPath: "completed")
+            tasks = []
+            allTasks.forEach{ task in
+                tasks.append(task)
+            }
+        }
+    }
+    
+    func updateTask(id: ObjectId, completed: Bool) {
+        if let localRealm = localRealm {
+            do {
+                let tasksToUpdate = localRealm.objects(Task.self).filter(NSPredicate(format: "id == %@", id))
+                guard !tasksToUpdate.isEmpty else { return }
+                try localRealm.write {
+                    tasksToUpdate[0].completed = completed
+                    getTasks()
+                    print("Updated task with id\(id)! Completed status: \(completed)")
+                }
+            } catch {
+                print("Error updating task \(id) to Realm: \(error)")
+            }
+        }
+    }
+}
--- ToDoList/ToDoList/Model/Task.swift
+++ ToDoList/ToDoList/Model/Task.swift
@@ -6,5 +6,12 @@
 //
 
 import Foundation
-import RealModule
+import RealmSwift
+
+class Task: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title = ""
+    @Persisted var completed = false
+}
+
 
